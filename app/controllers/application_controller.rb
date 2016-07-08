57fe664d33602de0681f49313b8a58dae526f503
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-07-08 16:24:47 +0200

    devise stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,35 @@
 class ApplicationController < ActionController::Base
+
+  # Forgery Protection
+
   protect_from_forgery with: :exception
+
+  # Filters
+
+  before_action :authenticate_user!
+  before_action :http_basic_auth
+
+
+  # Methods
+
+  protected
+
+  def render_404
+    raise ActionController::RoutingError.new('Not Found')
+  end
+
+  def set_flash_now_errors(object)
+    if object.errors.any?
+      flash.now[:alert] = object.errors.messages.values.join('<br />').html_safe
+    end
+  end
+
+  def http_basic_auth
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['HTTP_BASIC_AUTH_USERNAME'] &&
+        password == ENV['HTTP_BASIC_AUTH_PASSWORD']
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
 
+  # Devise
+
+  devise_for :users, controllers: {
+    sessions:  'users/sessions',
+    passwords: 'users/passwords'
+  }
+
+  # App
+
   resources :contacts
+
+  # Root
+
+  root to: 'contacts#index'
+
+  # Admin
+
+  # API
 
 end
