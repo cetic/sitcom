ee4edecf7e260fac6fe0f4cef1fd7e2850702862
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-07-13 13:34:57 +0200

    sitecom

--- config/initializers/00constants.rb
+++ config/initializers/00constants.rb
@@ -1,8 +1,8 @@
 if Rails.env.development?
   if ENV['USER'] == 'aurels'
     APP_PORT     = 80
-    APP_HOSTNAME = "lliw.dev"
-    MAIL_FROM    = 'noreply@lliw.dev'
+    APP_HOSTNAME = "sitcom.dev"
+    MAIL_FROM    = 'noreply@sitcom.dev'
   else
     APP_PORT     = 3000
     APP_HOSTNAME = "localhost:#{APP_PORT}"
@@ -11,6 +11,7 @@ if Rails.env.development?
 end
 
 if Rails.env.production?
-  APP_HOSTNAME = 'FIXME-lliw.be'
-  MAIL_FROM    = 'noreply@FIXME-lliw.be'
+  APP_PORT     = 443
+  APP_HOSTNAME = 'sitecom.cetic.be'
+  MAIL_FROM    = 'noreply@sitecom.cetic.be'
 end
