08225023d7312c44169013a579ade01aa0a3e927
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-07-14 15:22:04 +0200

    cookie name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_sitcom_session'
+Rails.application.config.session_store :cookie_store, key: "_sitcom_session_#{Rails.env}"
