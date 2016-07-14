2dff7d850f8c4336a268eada1409cd47b2a80cfb
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-07-14 16:23:55 +0200

    session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_sitcom_session_#{Rails.env}"
+Rails.application.config.session_store :cookie_store, key: "_sitcom_session_#{Rails.env}",
+                                                      secure: true
