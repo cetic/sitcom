c1e1bf0bb86509c3e38090274b634fb4a4f0c258
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-07-14 10:56:48 +0200

    config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,8 @@
-server ENV['DEPLOY_HOSTNAME'], user: 'deploy', roles: %w{web app db}
+server ENV['DEPLOY_HOSTNAME'], port:  ENV['DEPLOY_SSH_PORT'],
+                               user:  ENV['DEPLOY_SSH_USER'],
+                               roles: %w{web app db}
 
-set :application,  'lliw'
-set :deploy_to,    '/home/deploy/apps/lliw'
+set :application,  'sitcom'
+set :deploy_to,    "/home/#{ENV['DEPLOY_SSH_USER']}/apps/sitcom"
 set :branch,       'master'
 set :rbenv_ruby,   File.read('.ruby-version').strip
