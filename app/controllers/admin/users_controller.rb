8ead98459ae50603ba2f84280d45dd51343b41eb
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-07-15 17:07:47 +0200

    début d'admin des users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,57 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :find_user,        :only => [ :edit, :update, :destroy ]
+
   def index
+    @users = User.order(:name)
+  end
+
+  def new
+    @user = User.new
+    render 'form'
+  end
+
+  def create
+    @user = User.new(strong_params)
+
+    if @user.save
+      redirect_to admin_users_path
+    else
+      set_flash_now_errors(@user)
+      render 'form'
+    end
+  end
+
+  def edit
+    render 'form'
+  end
+
+  def update
+    if @user.update_attributes(strong_params)
+      redirect_to admin_users_path
+    else
+      set_flash_now_errors(@user)
+      render 'form'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  protected
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def strong_params
+    if params[:action] == 'create'
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    else
+      params.require(:user).permit(:name, :email)
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def f_date(date, over = false)
+    content = localize(date, :format => '%d/%m/%Y')
+    over ? content_tag(:span, content, { style: 'color:red' }) : content
+  end
+
+  def f_date_without_year(date, over = false)
+    localize(date, :format => '%d/%m')
+  end
+
+  def fa(name, suffix = nil)
+    if suffix.present?
+      "#{content_tag(:i, '', :class => "fa fa-#{name}")}&nbsp;#{suffix}".html_safe
+    else
+      content_tag(:i, '', :class => "fa fa-#{name}")
+    end
+  end
 end
