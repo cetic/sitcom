4be9190fabf97c6b15ff16b7071821db42ff40cc
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-07-21 16:16:40 +0200

    fix devise _after paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,4 +40,15 @@ class ApplicationController < ActionController::Base
       end
     end
   end
+
+  # Devise hooks
+
+  def after_sign_in_path_for(resource)
+    main_root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    main_root_path
+  end
+
 end
