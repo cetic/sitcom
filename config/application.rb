ae3c3016d574a1528f607007e000c96f3e662c65
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-08-04 17:03:04 +0200

    forgery_protection_origin_check

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Sitcom
     config.time_zone              = 'Brussels'
     config.i18n.available_locales = [:fr]
     config.i18n.default_locale    = :fr
+
+    config.action_controller.forgery_protection_origin_check = false
   end
 end
