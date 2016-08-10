b3c217ea28612cc59db07274614b089e11e4170a
Michaël Hoste <michael.hoste@gmail.com>
2016-08-10 16:50:56 +0200

    find_lab in organization

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,6 @@
 class OrganizationsController < ApplicationController
+  before_action :find_lab
+
   def index
     respond_to do |format|
       format.html
@@ -13,5 +15,12 @@ class OrganizationsController < ApplicationController
         }
       end
     end
+  end
+
+  private
+
+  def find_lab
+    @lab = current_user.labs.find_by_slug!(params[:lab_id])
+    save_lab_in_cookies(@lab)
   end
 end
