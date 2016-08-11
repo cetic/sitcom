26fa025e2b3dfd7bdf8be2ce6fd60129f58f89f9
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-08-11 15:50:27 +0200

    contacts

--- app/assets/javascripts/contacts/index.js.jsx
+++ app/assets/javascripts/contacts/index.js.jsx
@@ -1,59 +1,16 @@
-import Contact     from './index/contact.js.jsx'
-import QuickSearch from './index/quick_search.js.jsx'
+import Contacts from './index/contacts.js.jsx'
 
 class ContactsIndex extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      contacts: [],
-      booted: false,
-      loading: true
-    };
-  }
-
-  componentDidMount() {
-    this.reloadFromBackend();
-  }
-
-  reloadFromBackend() {
-    $.get(this.props.contactsPath, function(data){
-      var camelData = humps.camelizeKeys(data);
-
-      this.setState({
-        contacts: camelData.contacts,
-        booted: true,
-        loading: false
-      });
-    }.bind(this));
+    this.state = {};
   }
 
   render() {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-4 pull-right" style={{ height: '400px', backgroundColor: 'white' }}>
-            Recherche avancée
-          </div>
-
-          <div className="col-md-8">
-            <QuickSearch />
-
-            <div className="contacts">
-              {this.renderContacts()}
-            </div>
-          </div>
-        </div>
-      </div>
+      <Contacts contactsPath={this.props.contactsPath} />
     );
-  }
-
-  renderContacts() {
-    return _.map(this.state.contacts, function(contact) {
-      return (
-        <Contact key={contact.id} contact={contact} />
-      );
-    });
   }
 }
 
---
+++ app/assets/javascripts/contacts/index/contacts.js.jsx
@@ -0,0 +1,60 @@
+import Contact     from './contact.js.jsx'
+import QuickSearch from './quick_search.js.jsx'
+
+class Contacts extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [],
+      booted: false,
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    this.reloadFromBackend();
+  }
+
+  reloadFromBackend() {
+    $.get(this.props.contactsPath, function(data){
+      var camelData = humps.camelizeKeys(data);
+
+      this.setState({
+        contacts: camelData.contacts,
+        booted: true,
+        loading: false
+      });
+    }.bind(this));
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-4 pull-right" style={{ height: '400px', backgroundColor: 'white' }}>
+            Recherche avancée
+          </div>
+
+          <div className="col-md-8">
+            <QuickSearch />
+
+            <div className="contacts">
+              {this.renderContacts()}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderContacts() {
+    return _.map(this.state.contacts, function(contact) {
+      return (
+        <Contact key={contact.id} contact={contact} />
+      );
+    });
+  }
+}
+
+module.exports = Contacts
