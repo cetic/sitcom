edc692f182375a26c6f5f2ca1f9de02f7a723f22
Michaël Hoste <michael.hoste@gmail.com>
2016-08-19 19:08:21 +0200

    simplify browserify

--- config/initializers/browserify.rb
+++ config/initializers/browserify.rb
@@ -1,6 +1,1 @@
 Rails.application.config.browserify_rails.commandline_options = "-t [ babelify --presets [ es2015 react ] --extensions .es6 ]"
-
-Rails.application.config.browserify_rails.paths = [
-  -> (p) { p.start_with? Rails.root.join("app/assets/javascripts").to_s },
-  -> (p) { p.start_with? Rails.root.join("node_modules").to_s }
-]
