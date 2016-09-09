3a0e516f776ff544ba35c89916766bf72516616e
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-09-09 16:55:16 +0200

    admin has full read/write access

--- app/models/lab_user_link.rb
+++ app/models/lab_user_link.rb
@@ -9,14 +9,14 @@ class LabUserLink < ApplicationRecord
 
   def permissions
     {
-      :can_read_contacts       => can_read_contacts,
-      :can_write_contacts      => can_write_contacts,
-      :can_read_organizations  => can_read_organizations,
-      :can_write_organizations => can_write_organizations,
-      :can_read_projects       => can_read_projects,
-      :can_write_projects      => can_write_projects,
-      :can_read_events         => can_read_events,
-      :can_write_events        => can_write_events
+      :can_read_contacts       => user.admin? || can_read_contacts,
+      :can_write_contacts      => user.admin? || can_write_contacts,
+      :can_read_organizations  => user.admin? || can_read_organizations,
+      :can_write_organizations => user.admin? || can_write_organizations,
+      :can_read_projects       => user.admin? || can_read_projects,
+      :can_write_projects      => user.admin? || can_write_projects,
+      :can_read_events         => user.admin? || can_read_events,
+      :can_write_events        => user.admin? || can_write_events
     }
   end
 end
