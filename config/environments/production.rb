ee103f96108890a7bf201701b6c9d4a0cc4182a0
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-09-30 15:06:02 +0200

    config.action_cable.url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,9 +37,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
 
-  config.action_cable.url                     = 'ws://sitcom.cetic.be'
-  config.action_cable.allowed_request_origins = [ 'https://sitcom.cetic.be' ]
-  config.action_cable.disable_request_forgery_protection = true
+  # config.action_cable.url                     = 'ws://sitcom.cetic.be'
+  # config.action_cable.allowed_request_origins = [ 'https://sitcom.cetic.be' ]
+  config.action_cable.allowed_request_origins   = [ '*' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
