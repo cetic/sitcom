390ca8a46ebbfe3a9c4db3f455e9da8aa06ce679
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-10-01 10:39:34 +0200

    allowed request origins from ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,8 +38,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
 
   # config.action_cable.url                     = 'ws://sitcom.cetic.be'
-  # config.action_cable.allowed_request_origins = [ 'https://sitcom.cetic.be' ]
-  # config.action_cable.allowed_request_origins   = [ /http:\/\/*.*/ ]
+  config.action_cable.allowed_request_origins   = ENV['ACTION_CABLE_ALLOWED'].split('|')
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
