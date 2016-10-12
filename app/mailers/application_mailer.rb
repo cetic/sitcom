55a69f9c5adc761f00280228437f3b41c05ade68
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-10-12 14:48:21 +0200

    add info mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,16 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: MAIL_FROM
   layout 'mailer'
+
+  def info(subject, body = "")
+    @body = body
+
+    mail({
+      :from    => MAIL_FROM,
+      :subject => "#{subject}",
+      :to => [
+        'aurelien.malisart@gmail.com',
+        'michael.hoste@gmail.com'
+      ]
+    })
+  end
 end
