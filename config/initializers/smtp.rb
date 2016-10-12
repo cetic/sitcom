577e3ec118a6b39ee62fe37ad588655ac2d07b52
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-10-12 10:46:43 +0200

    smtp

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,12 +1,12 @@
 Rails.application.configure do
-  if ENV['SMTP_DOMAIN']
+  if ENV['SMTP_ADDRESS']
     config.action_mailer.smtp_settings = {
-      :domain               => ENV['SMTP_DOMAIN'],
-      :address              => ENV['SMTP_ADDRESS'],
-      :port                 => ENV['SMTP_PORT'].to_i,
+      :domain               => ENV['SMTP_DOMAIN'  ],
+      :address              => ENV['SMTP_ADDRESS' ],
+      :port                 => ENV['SMTP_PORT'    ].to_i,
       :user_name            => ENV['SMTP_USERNAME'],
       :password             => ENV['SMTP_PASSWORD'],
-      :authentication       => :plain,
+      :authentication       => :login,
       :enable_starttls_auto => true
     }
   end
