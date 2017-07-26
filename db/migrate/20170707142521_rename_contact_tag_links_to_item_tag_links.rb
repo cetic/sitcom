93d6fbe188ad07c9057a3ea4d1bf717623fd4fab
Michaël Hoste <michael.hoste@gmail.com>
2017-07-26 16:35:14 +0200

    fix migration for item_tags

--- db/migrate/20170707142521_rename_contact_tag_links_to_item_tag_links.rb
+++ db/migrate/20170707142521_rename_contact_tag_links_to_item_tag_links.rb
@@ -3,6 +3,8 @@ class RenameContactTagLinksToItemTagLinks < ActiveRecord::Migration[5.0]
     #rename_table :contact_tag_links, :item_tag_links
 
     #add_column    :item_tag_links, :item_type,  :string,  :after => :contact_id, :default => 'Contact', :index => true
-    rename_column :item_tag_links, :contact_id, :item_id
+
+    remove_foreign_key :item_tag_links, :column => :contact_id
+    rename_column      :item_tag_links, :contact_id, :item_id
   end
 end
