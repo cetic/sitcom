6ae450407ca03f8dac1ac40f66061e26243b4966
Aurélien Malisart <aurelien.malisart@gmail.com>
2020-05-22 09:48:14 +0200

    prepare for production

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,7 +1,5 @@
-BUGSNAG_API_KEY = '9731783e2ea10c1b5be62716ff0c57b6'
-
 Bugsnag.configure do |config|
-  config.api_key               = BUGSNAG_API_KEY
+  config.api_key               = ENV['BUGSNAG_API_KEY']
   config.notify_release_stages = ['production', 'staging']
   config.send_environment      = true
 end
